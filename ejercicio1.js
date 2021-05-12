d69884c55c1b330910ee0abffb7a021933501f90
AlejandroSevillaJimenez <75426906+AlejandroSevillaJimenez@users.noreply.github.com>
2021-05-12 09:50:44 +0200

    Update ejercicio1.js

--- ejercicio1.js
+++ ejercicio1.js
@@ -6,11 +6,5 @@ function gestionarFicheroXML(xmlDoc){
 	let tabla = document.querySelector("tabla");
 	let libros = xmlDoc.querySelectorAll("libro");
 	for(let i=0; i<libros.length; i++)
-		tabla.innerHTML += "<div class = 'fila'>" + libros[i].textContent + "<div class = 'celda'>";
-}
-
-capa.addEventListener("click",CargarFichero);
-function CargarFichero()
-{
-	loadDocA("leerFicheroXML.xml","xml");
+		tabla.innerHTML += "<div class = 'fila'>" + libros[i].textContent + "<div class = 'celda'>" + libros[i].querySelector("ISBN");
 }
